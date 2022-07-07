f2b29327803e0444a921304efa7801d53a13ef0d
dev1 <dev1@gmail.com>
2022-07-07 06:20:41 +0530

    Adding example and significance of shebang in programs

---
+++ Basics/Shebang/with_shebang.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+"""
+#! is a shebang character sequence
+after that absolute path of the interpreter
+This will let the shell understand that this has to be interpreted using python3 from the path mentioned so
+we don't have to explicitly mention   " python3 file_name.py "    to execute.
+We can just use " ./file_name.py " to execute.
+
+In scripting languages # is considered as a comment but #! is a special sequence which will be understood
+by the shell itself.
+
+In some cases executable permission won't be available for files by default so use " chmod +x filename.py " command
+to give executable permission before executing.
+
+This works only in linux platforms. Sorry windows users :(
+
+"""
+print("Life is beautiful")
